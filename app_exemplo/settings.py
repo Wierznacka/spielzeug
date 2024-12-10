52406c2f49110df8439c1641bee4d376d4692ce8
Wierznacka <marcialeal.inf@gmail.com>
2024-12-10 19:41:29 -0300

    Du Hast

--- app_exemplo/settings.py
+++ app_exemplo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+
 SECRET_KEY = 'django-insecure-3-f3mm@72sz#*&bo4sb0=j_vc7nych_sn0#zxic0&m**)vg($3'
 DEBUG = True
 
